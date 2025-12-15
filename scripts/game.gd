a242c10cc34f13fb6200eab70bed50b528e6ad88
MobsterTheLobster <lobstr.mobstr@gmail.com>
2025-12-15 03:48:26 +0000

    initial commit but fr this time

---
+++ scripts/game.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+func _ready() -> void:
+	var xr_interface = XRServer.find_interface("OpenXR")
+	
+	if xr_interface and xr_interface.is_initialized():
+		print("yeah it worked")
+		
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		
+		get_viewport().user_xr = true
+	else:
+		print("did not work. where headset?")
